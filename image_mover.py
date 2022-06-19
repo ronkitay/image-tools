6bdb7dec52e6f1fd6e2ef289eaa0275b6c72faf5
Ron Kitay <ronk@qwilt.com>
2022-06-19 08:19:09 +0300

    update image_mover.py

--- image_mover.py
+++ image_mover.py
@@ -6,8 +6,8 @@ def is_image(file_name):
     file_name = file_name.lower()
     return file_name.endswith(".jpg") or file_name.endswith(".jpeg") or file_name.endswith(".gif")
 
-old_dir_name = '/Users/rkitay/Pictures/Sorting/from_onedrive__likely_dups/DCIM/100MEDIA'
-new_root_dir = '/Users/rkitay/Pictures/Sorting/from_onedrive__likely_dups/DCIM/100MEDIA/split'
+old_dir_name = '/Users/ronk/OneDrive/Photos/_Exported From Google Photos/takout/2018/01'
+new_root_dir = '/Users/ronk/OneDrive/Photos/_Exported From Google Photos/takout/2018/01'
 num_of_files_moved = 0
 
 
@@ -16,7 +16,7 @@ def is_movie(file_name):
     return file_name.endswith(".avi") or file_name.endswith(".mov") or file_name.endswith(".mp4") or file_name.endswith(".mpeg")
 
 DEMO = True
-DEMO = False
+# DEMO = False
 
 
 def get_year(file_name):
@@ -29,7 +29,7 @@ def get_month(file_name):
 
 def validate_year_and_month(year, month):
     year_int = int(year)
-    if year_int > 2017:
+    if year_int > 2021:
         raise ValueError("Year cannot be {}".format(year_int))
     if year_int < 2011:
         raise ValueError("Year cannot be {}".format(year_int))
@@ -41,32 +41,31 @@ def validate_year_and_month(year, month):
         raise ValueError("Month cannot be {}".format(month_int))
 
 
-for file_name in os.listdir(old_dir_name):
-    if file_name.startswith('IMG') or file_name.startswith('201'):
-        if is_image(file_name) or is_movie(file_name):  # and not file_name.__contains__('-'):
-            # if '2016-09' in file_name:
-            print "working on file: %s" % file_name
-            year = get_year(file_name)
-            month = get_month(file_name)
+if __name__ == '__main__':
 
-            validate_year_and_month(year, month)
-            # print "year = %s" % (year)
-            # print "month = %s" % (month)
-            target_dir = new_root_dir + '/' + year + '/' + month
-            print "target dir = %s" % target_dir
-            if not DEMO:
-                if not os.path.exists(target_dir):
-                    os.makedirs(target_dir)
-            num_of_files_moved += 1
-            print ("Renaming {} to {}".format(old_dir_name + '/' + file_name, target_dir + '/' + file_name))
-            if not DEMO:
-                os.rename(old_dir_name + '/' + file_name, target_dir + '/' + file_name)
+    for file_name in os.listdir(old_dir_name):
+        if file_name.startswith('IMG') or file_name.startswith('201'):
+            if is_image(file_name) or is_movie(file_name):  # and not file_name.__contains__('-'):
+                print("working on file: {}".format(file_name))
+                year = get_year(file_name)
+                month = get_month(file_name)
 
-            continue
-        else:
-            continue
+                validate_year_and_month(year, month)
+                target_dir = new_root_dir + '/' + year + '/' + month
+                print("target dir = {}".format(target_dir))
+                # if not DEMO:
+                #     if not os.path.exists(target_dir):
+                #         os.makedirs(target_dir)
+                num_of_files_moved += 1
+                print("Renaming {} to {}".format(old_dir_name + '/' + file_name, target_dir + '/' + file_name))
+                # if not DEMO:
+                #     os.rename(old_dir_name + '/' + file_name, target_dir + '/' + file_name)
 
-print ("num_of_files_moved = {}".format(num_of_files_moved))
+                continue
+            else:
+                continue
+
+    print("num_of_files_moved = {}".format(num_of_files_moved))
 
 
 
