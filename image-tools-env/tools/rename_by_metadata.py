94be92a41705206bf04fc9d27ab59dd11ee0d64f
Ron Kitay <ronk@qwilt.com>
2023-03-08 12:24:18 +0200

    some refactoring in rename_by_metadata and move scripts in image from /root/image-tools to /root/tools

--- image-tools-env/tools/rename_by_metadata.py
+++ image-tools-env/tools/rename_by_metadata.py
@@ -8,6 +8,9 @@ import argparse
 from exifread import process_file
 import ffmpeg
 
+from zoneinfo import ZoneInfo
+from dateutil import parser
+
 image_extensions = {"jpg", "jpeg", "gif"}
 video_extensions = {"mp4", "mov", "avi"}
 
@@ -25,36 +28,16 @@ def _rename_files_based_on_metadata(directory, debug_mode):
                 directory, file_name, debug_mode, metadata_extractor)
 
 
-def _rename_based_on_metadata(directory, file_name, debug_mode, metadata_extractor):
-    original_file_name = os.path.join(directory, file_name)
-
-    file_time_stamp = metadata_extractor(original_file_name)
-
-    if file_time_stamp:
-        new_file_name = os.path.join(
-            directory, '{}.{}'.format(file_time_stamp, _get_extension(file_name)))
-
-        if new_file_name == original_file_name:
-            print('{} already has the correct name, skipping'.format(
-                original_file_name))
-            return
-
-        while os.path.exists(new_file_name):
-            # print("{} already exists, trying {}".format(
-            #     new_file_name, new_file_name+"-1"))
-            # new_file_name = new_file_name+"-1"
-            file_time_stamp = file_time_stamp + "-1"
-            new_file_name = os.path.join(
-                directory, '{}.{}'.format(file_time_stamp, _get_extension(file_name)))
-
-        print("Renaming {} to be {}".format(
-            original_file_name, new_file_name))
-        if not debug_mode:
-            os.rename(original_file_name, new_file_name)
-
-
 def _is_image(file_name):
-    return _get_extension(file_name) in image_extensions
+    return _matches_extension(file_name, image_extensions)
+
+
+def _is_video(file_name):
+    return _matches_extension(file_name, video_extensions)
+
+
+def _matches_extension(file_name, extensions):
+    return _get_extension(file_name) in extensions
 
 
 def _get_extension(file_name):
@@ -62,15 +45,7 @@ def _get_extension(file_name):
 
 
 def _extract_metadata_from_image_file(full_file_name):
-    raw_original_date_time = None
-    f = open(full_file_name, 'rb')
-    tags = process_file(f)
-    if tags.__contains__("EXIF DateTimeOriginal"):
-        raw_original_date_time = tags["EXIF DateTimeOriginal"].printable
-    elif tags.__contains__("Image DateTime"):
-        raw_original_date_time = tags["Image DateTime"].printable
-    else:
-        print("No date/time in tags for {}".format(full_file_name))
+    raw_original_date_time = _extract_date_time_from_image_file(full_file_name)
 
     if raw_original_date_time:
         formatted_date = raw_original_date_time.replace(
@@ -86,11 +61,33 @@ def _extract_metadata_from_image_file(full_file_name):
         return None
 
 
-def _is_video(file_name):
-    return _get_extension(file_name) in video_extensions
+def _extract_date_time_from_image_file(full_file_name):
+    f = open(full_file_name, 'rb')
+    tags = process_file(f)
+    if tags.__contains__("EXIF DateTimeOriginal"):
+        return tags["EXIF DateTimeOriginal"].printable
+    elif tags.__contains__("Image DateTime"):
+        return tags["Image DateTime"].printable
+    else:
+        return None
 
 
 def _extract_metadata_from_video_file(full_file_name):
+    timestamp = _extract_timestamp_from_video_file(full_file_name)
+
+    if timestamp:
+        year = timestamp.year
+        month = timestamp.month
+        day = timestamp.day
+        hour = timestamp.hour
+        minute = timestamp.minute
+        second = timestamp.second
+        return '{}-{:02d}-{:02d} {:02d}.{:02d}.{:02d}'.format(year, month, day, hour, minute, second)
+    else:
+        return None
+
+
+def _extract_timestamp_from_video_file(full_file_name):
     all_metadata = ffmpeg.probe(full_file_name)
 
     if 'format' in all_metadata:
@@ -98,24 +95,44 @@ def _extract_metadata_from_video_file(full_file_name):
         if 'tags' in format:
             tags = format['tags']
             if 'creation_time' in tags:
-                from zoneinfo import ZoneInfo
-                # print(tags['creation_time'])
-                from dateutil import parser
                 parsered_date = parser.isoparse(tags['creation_time'])
-                # print(parsered_date)
-                timestamp = parsered_date.astimezone(
-                    ZoneInfo('Asia/Jerusalem'))
-                # timestamp = tags['creation_time'].replace('-', '').replace(
-                #     ':', '').replace(' ', '').replace('T', '').split('.')[0]
-                year = timestamp.year
-                month = timestamp.month
-                day = timestamp.day
-                hour = timestamp.hour
-                minute = timestamp.minute
-                second = timestamp.second
-                return '{}-{:02d}-{:02d} {:02d}.{:02d}.{:02d}'.format(year, month, day, hour, minute, second)
+                return parsered_date.astimezone(ZoneInfo('Asia/Jerusalem'))
 
     return None
+
+
+def _rename_based_on_metadata(directory, file_name, debug_mode, metadata_extractor):
+    original_file_name = os.path.join(directory, file_name)
+
+    file_time_stamp = metadata_extractor(original_file_name)
+
+    if file_time_stamp:
+        extension = _get_extension(file_name)
+        new_file_name = _get_new_file_name(
+            directory, file_time_stamp, extension)
+
+        if new_file_name == original_file_name:
+            print('{} already has the correct name, skipping'.format(
+                original_file_name))
+            return
+
+        while os.path.exists(new_file_name):
+            print("{} already exists, adding a suffix".format(new_file_name))
+            file_time_stamp = file_time_stamp + "-1"
+            new_file_name = _get_new_file_name(
+                directory, file_time_stamp, extension)
+
+        print("Renaming {} to be {}".format(
+            original_file_name, new_file_name))
+        if not debug_mode:
+            os.rename(original_file_name, new_file_name)
+    else:
+        print("No date/time in tags for {}".format(original_file_name))
+
+
+def _get_new_file_name(directory, file_timestamp, extension):
+    new_file_name = '{}.{}'.format(file_timestamp, extension)
+    return os.path.join(directory, new_file_name)
 
 
 def main(args):
